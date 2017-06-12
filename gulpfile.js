cda36772eff127cf085723b6aef6df92968c9296
Xavier Xie <xieyuehao@gmail.com>
2017-06-12 23:39:09 +1000

    FINISH up to Templating engines

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon =  require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -18,6 +19,17 @@ gulp.task('style', function () {
 gulp.task('inject', function () {
     //it looks the bower.js file and look for the dependencies
     var wiredep = require('wiredep').stream;
+
+    var inject = require("gulp-inject");
+    var injectSrc = gulp.src([
+                                "./public/css/*.css",
+                                "./public/js/*.js"
+                                ], { read:false});
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
@@ -26,5 +38,22 @@ gulp.task('inject', function () {
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+})
+
+//this will run style and inject tasks before run serve task
+gulp.task('serve', ['style','inject'], function () {
+        var options = {
+                script: 'app.js',
+                delayTime: 1,
+                env:{
+                    'PORT': 3000
+                },
+                watch: jsFiles
+        }
+
+        return nodemon(options).on('restart', function(ev){
+                console.log("Restarting....");
+        });
 })
